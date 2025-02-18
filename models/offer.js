60ce642ce29372868f8e7ab7f7f77a4fa4c21488
Diluk Mihiranga <dilukedu@gmail.com>
2025-02-18 13:07:15 +0530

    feat: enhance offer model with additional fields and standardize string quotes

--- models/offer.js
+++ models/offer.js
@@ -1,31 +1,43 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const OfferSchema = new Schema({
   title: {
     type: String,
-    required: [true, 'Please add a title'],
+    required: [true, "Please add a title"],
     trim: true,
-    maxlength: [100, 'Title can not be more than 100 characters']
+    maxlength: [100, "Title can not be more than 100 characters"],
   },
   description: {
     type: String,
-    required: [true, 'Please add a description']
+    required: [true, "Please add a description"],
   },
   discount: {
     type: Number,
     required: true,
-    min: [0, 'Discount can not be negative'],
-    max: [100, 'Discount can not be more than 100%']
+    min: [0, "Discount can not be negative"],
+    max: [100, "Discount can not be more than 100%"],
   },
   shop: {
     type: Schema.ObjectId,
-    ref: 'Shop',
-    required: true
+    ref: "Shop",
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: [true, "Please add a start date"],
+  },
+  endDate: {
+    type: Date,
+    required: [true, "Please add an end date"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Please add an image URL"],
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-export default model('Offer', OfferSchema);
+export default model("Offer", OfferSchema);
