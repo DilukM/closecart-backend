a6d7fb1cf07ac7ad21be325232603113054da77f
Diluk Mihiranga <dilukedu@gmail.com>
2025-05-14 21:10:52 +0530

    fix: enable route protection for geocoding routes

--- routes/geocodingRoutes.js
+++ routes/geocodingRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { getAddressFromCoords, getCoordsFromAddress } from "../controllers/geocodingController.js";
+import {
+  getAddressFromCoords,
+  getCoordsFromAddress,
+} from "../controllers/geocodingController.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Protect all routes
-router.use(protect);
 
 // Geocoding routes
 router.get("/reverse", getAddressFromCoords);
