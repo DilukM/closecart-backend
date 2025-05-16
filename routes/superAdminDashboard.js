65cbec435142922b50e58c7c06c533062d012864
Diluk Mihiranga <dilukedu@gmail.com>
2025-05-16 15:30:44 +0530

    feat: add route logging middleware and enhance CSV import logging for better debugging

--- routes/superAdminDashboard.js
+++ routes/superAdminDashboard.js
@@ -14,6 +14,16 @@ import { protectSuperAdmin } from "../middleware/superAdminAuth.js";
 
 const router = express.Router();
 
+// Route logging middleware
+const logRoute = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  console.log("Request headers:", req.headers);
+  next();
+};
+
+// Apply route logging to all routes
+router.use(logRoute);
+
 // Apply protection to all routes
 // router.use(protectSuperAdmin);
 
@@ -27,7 +37,15 @@ router.route("/offers").get(getOffers).post(createOffer);
 
 router.route("/offers/:id").put(updateOffer).delete(deleteOffer);
 
-// CSV Import route
-router.route("/import/csv").post(importFromCSV);
+// CSV Import route with additional logging
+router.route("/import/csv").post((req, res, next) => {
+  console.log("CSV import request received");
+  console.log("Content-Type:", req.headers["content-type"]);
+  console.log("Request has files object:", !!req.files);
+  if (req.files) {
+    console.log("Files keys:", Object.keys(req.files));
+  }
+  next();
+}, importFromCSV);
 
 export default router;
