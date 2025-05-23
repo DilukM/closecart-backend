7f648885c9fcde6cc32a30432fd47494c183a1da
Diluk Mihiranga <dilukedu@gmail.com>
2025-05-23 22:10:02 +0530

    fix: enhance logging for liked shops route to improve request tracking

--- routes/consumer.js
+++ routes/consumer.js
@@ -27,7 +27,6 @@ import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
-
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/google-signin", googleSignIn);
@@ -38,8 +37,20 @@ router.post("/upload-image/:id", protect, uploadProfileImage);
 router.delete("/delete-profile", protect, deleteProfile);
 
 router.get("/liked-shops/:id", protect, getLikedShops);
-router.post("/liked-shops", protect,addLikedShop);
-router.delete("/liked-shops", protect,deleteLikedShop);
+router.post(
+  "/liked-shops",
+  protect,
+  (req, res, next) => {
+    console.log("------------------------------------");
+    console.log(`POST request to /liked-shops at ${new Date().toISOString()}`);
+    console.log("Request body:", req.body);
+    console.log("User:", req.user);
+    console.log("------------------------------------");
+    next();
+  },
+  addLikedShop
+);
+router.delete("/liked-shops", protect, deleteLikedShop);
 
 router.get("/", getAllProfiles);
 router.get("/:id", getProfile);
